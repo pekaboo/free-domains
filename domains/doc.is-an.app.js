f883c145bea1987feead631c43df21c0f0743de6
学长的猫 <me.wangjun@qq.com>
2024-03-23 17:55:01 +0800

    add a domain

---
+++ domains/doc.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A website for my vercel doc',
+  domain: 'is-an.app',
+  subdomain: 'doc',
+  owner: {
+    email: 'pekaboo@qq.com',
+  },
+  record: {
+    CNAME: 'cname.vercel-dns.com'
+  },
+  proxy: false
+})
